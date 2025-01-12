ef6b96cbf9e56c8cff787510d0ecb38583d2da05
asif883 <asifiqbal95863@gmail.com>
2025-01-12 15:15:47 +0600

    Connet to MongoDB and create routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config()
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+
 const port = process.env.PORT || 3000
 
 
@@ -10,6 +12,54 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osztyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+   const dailyCostCollection = client.db("Hostel-manager").collection("dailyCost")
+
+  const dbConnect = async()=>{
+    try{
+        await client.connect()
+        console.log('DB Connected');
+
+
+       // Routes
+       app.post('/add-daily-cost', async(req , res)=>{
+            const cost = req.body
+            const result = await dailyCostCollection.insertOne(cost)
+            res.send(result)
+       }) 
+       
+       app.get('/daily-cost' , async( req, res )=>{
+           const dailyCost = await dailyCostCollection.find().toArray()
+
+           const totalCost = dailyCost.reduce((total, item) => {
+                const cost = parseFloat(item.cost) || 0;
+                return total + cost;
+            }, 0)
+           res.send({dailyCost, totalCost})
+       })
+
+       app.delete('/daily-cost/:id', async(req , res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result =await dailyCostCollection.deleteOne(query);
+        res.send(result);
+      })
+
+    }
+    catch(err){
+      console.log(err.massage);
+    }
+  }
+ dbConnect()
 
 app.get('/' , async (req , res)=>{
     res.send('server is running')
